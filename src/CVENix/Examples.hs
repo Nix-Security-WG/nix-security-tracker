cbb38d9a0a8dc923c99e67ebdbbc946805906095
Arnout Engelen <arnout@bzzt.net>
2023-11-20 16:34:56 +0100

    Prin advisory matches
    Now produces a result like:
    
    ```
    Matched advisories:
    "avahi"	"/nix/store/08xh2gdj9ji94fcjpf2hpz42jim99kkp-avahi-0.8.drv"	"CVE-2023-38473"
    "gawk"	"/nix/store/jzc31i1d1mi6x2rmp9pj6dcvw2mx7daz-gawk-5.2.2.drv"	"CVE-2023-4156"
    "glib"	"/nix/store/09yqd7yg7rfjsfbikvc3snx7wn9c6is5-glib-2.76.4.drv"	"CVE-2023-29499"
    "glibc"	"/nix/store/30j4y2k04zww68mggx1mkmmi6b9ns2yp-glibc-2.38-27.drv"	"CVE-2023-5156"
    "glibc"	"/nix/store/3affpfhnsgifgqkhd1zckk527a86fyrz-glibc-locales-2.38-27.drv"	"CVE-2023-5156"
    "kernel"	"/nix/store/h3sqgjbm7a3gvyn50r5k7066xjhgn5iy-kernel-modules.drv"	"CVE-2023-6176"
    "libX11"	"/nix/store/wxd38000dmzrd51nl4d0q88c3xximhlr-libX11-1.8.7.drv"	"CVE-2023-43787"
    "libXpm"	"/nix/store/a66y6hxcv8qcal43i49nvrzfi7k9jj12-libXpm-3.5.17.drv"	"CVE-2023-43789"
    "libtiff"	"/nix/store/c80az5882p200wyi9vk5jkbrqf8wi2q1-libtiff-4.6.0.drv"	"CVE-2023-41175"
    "w3m"	"/nix/store/0v1i0bx7ld6lh0g9kkykc08in4y9612w-w3m-0.5.3+git20230121.drv"	"CVE-2023-38253"
    ```

--- src/CVENix/Examples.hs
+++ src/CVENix/Examples.hs
@@ -9,7 +9,12 @@ import Data.Time.Clock
 import Data.Maybe
 import Data.Text (Text)
 
-exampleParseCVE :: IO [Text]
+data Advisory = Advisory
+  { _advisory_cveId :: Text
+  , _advisory_productName :: Maybe Text
+  } deriving Show
+
+exampleParseCVE :: IO [Advisory]
 exampleParseCVE = do
     files' <- listDirectory "CVE/cves/"
     let files = filter (\x -> not (x == "delta.json" || x == "deltaLog.json")) files'
@@ -27,7 +32,7 @@ exampleParseCVE = do
     curTime <- getCurrentTime
     l <- flip mapM thing' $ \x -> do
       file <- decodeFileStrict x :: IO (Maybe CVE)
-      pure $ getCVEIDs file
+      pure $ asAdvisories file
     l2 <- flip mapM thing' $ \x -> do
       file <- decodeFileStrict x
       pure $ getCPEIDs file
@@ -35,15 +40,20 @@ exampleParseCVE = do
     curTime' <- getCurrentTime
     putStrLn $ "[CVE] Time to run: " <> (show $ diffUTCTime curTime curTime' * (-1))
     print $ filter (\a -> _cpe_vendor a /= "microsoft") $ catMaybes $ map parseCPE (concat $ concat $ l2)
-    pure $ catMaybes $ concat $ l
+    pure $ concat $ l
   where
-      getCVEIDs p = case p of
+      asAdvisories :: Maybe CVE -> [Advisory]
+      asAdvisories p = case p of
                       Nothing -> []
                       Just cve -> do
-                          let unwrappedContainer = _cna_affected $ _container_cna $ _cve_containers cve
+                          let
+                            cveId = _cvemetadata_cveId $ _cve_cveMetadata cve
+                            unwrappedContainer = _cna_affected $ _container_cna $ _cve_containers cve
                           case unwrappedContainer of
                             Nothing -> []
-                            Just a -> map (_product_packageName) a
+                            Just a ->
+                              let names = mapMaybe (_product_packageName) a
+                              in map (\n -> Advisory {_advisory_cveId = cveId, _advisory_productName = Just n}) names
       getCPEIDs p = case p of
                       Nothing -> []
                       Just cve -> do
