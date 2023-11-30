d3c5df1fe7a5a8872b6880e8f0b2089c94e26b16
cidkidnix <cidkidnix@protonmail.com>
2023-11-30 10:20:01 -0600

    Run Sbom nix within code

--- exe/Main.hs
+++ exe/Main.hs
@@ -8,10 +8,13 @@ import Options.Applicative
 import CVENix.SBOM
 import CVENix.Examples
 import CVENix.Matching
+import System.Process
+import System.Posix.Files
 
 data Parameters = Parameters
   { debug :: !Bool
   , sbom :: !String
+  , path :: !(Maybe String)
   } deriving (Show, Eq, Ord)
 
 programOptions :: Parser Parameters
@@ -20,12 +23,15 @@ programOptions = Parameters
              <> short 'v'
              <> help "Debug logging"
              )
-  <*> strOption (  long "drv"
+  <*> strOption (  long "sbom"
                 <> value "sbom.cdx.json"
                 <> help "SBOM to ingest"
                 <> metavar "SBOM JSON"
                 <> showDefault
                 )
+  <*> (optional $ strOption ( long "path"
+                <> help "Path to ingest"
+                ))
 
 parameterInfo :: ParserInfo Parameters
 parameterInfo = info (helper <*> programOptions) (fullDesc <> progDesc "Nix Security Scanner" <> header "Nix Security Scanner")
@@ -34,10 +40,23 @@ parameterInfo = info (helper <*> programOptions) (fullDesc <> progDesc "Nix Secu
 main :: IO ()
 main = do
     params <- execParser parameterInfo
-    sbom' <- parseSBOM $ sbom params
-    cves <- exampleParseCVE
-    case sbom' of
-      Nothing ->
-        putStrLn "[SBOM] Failed to parse"
-      Just s ->
-        match s cves $ (debug params)
+    exists <- fileExist (sbom params)
+    case exists of
+      True -> go params
+      False -> case (path params) of
+          Just drv' -> do
+            callProcess "sbomnix" [drv', "--type", "runtime"]
+            go params
+          _ -> error "Please specify drv file"
+
+ where
+     go params = do
+       let sbom' = sbom params
+           debug' = debug params
+       sbom'' <- parseSBOM $ sbom'
+       cves <- exampleParseCVE
+       case sbom'' of
+         Nothing ->
+           putStrLn "[SBOM] Failed to parse"
+         Just s ->
+           match s cves $ debug'
