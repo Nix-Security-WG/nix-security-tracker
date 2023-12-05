fb85b39186c837ea74772213291a5586b41ff714
fricklerhandwerk <valentin@fricklerhandwerk.de>
2023-12-05 03:49:29 +0100

    feat(routing): use same URL prefix for list and detail view
    this allows simply appending the issue code to get to the detail view

--- src/website/webview/urls.py
+++ src/website/webview/urls.py
@@ -7,9 +7,9 @@ app_name = "webview"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
-    path("issues", NixpkgsIssueListView.as_view(), name="issue_list"),
+    path("issues/", NixpkgsIssueListView.as_view(), name="issue_list"),
     re_path(
-        r"^issue/(?P<code>NIXPKGS-[0-9]{4}-[0-9]{4,19})$",
+        r"^issues/(?P<code>NIXPKGS-[0-9]{4}-[0-9]{4,19})$",
         NixpkgsIssueView.as_view(),
         name="issue_detail",
     ),
