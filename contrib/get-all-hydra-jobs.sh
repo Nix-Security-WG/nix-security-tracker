b77fdd1fe3b7f789005e47b5e608e3281c8eb91d
Raito Bezarius <masterancpp@gmail.com>
2023-12-10 15:51:40 +0100

    fix: evaluate insecure packages (for real, this time)
    b008bd5d1 was an incomplete fix to allow insecure packages evaluation.
    The insecure exception must be provided in the instantiation of the nixpkgs set.

--- contrib/get-all-hydra-jobs.sh
+++ contrib/get-all-hydra-jobs.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-NIXPKGS_ALLOW_INSECURE=1 nix-eval-jobs --force-recurse --meta --repair --quiet --gc-roots-dir /tmp/gcroots --expr "(import <nixpkgs/pkgs/top-level/release.nix> { })" --include nixpkgs=$LOCAL_NIXPKGS_CHECKOUT "$@" > evaluation.jsonl
+NIXPKGS_ARGS="{ config = { allowUnfree = true; inHydra = false; allowInsecurePredicate = (_: true); scrubJobs = false; }; };"
+nix-eval-jobs --force-recurse --meta --repair --quiet --gc-roots-dir /tmp/gcroots --expr "(import <nixpkgs/pkgs/top-level/release.nix> { nixpkgsArgs = $NIXPKGS_ARGS })" --include nixpkgs=$LOCAL_NIXPKGS_CHECKOUT "$@" > evaluation.jsonl
