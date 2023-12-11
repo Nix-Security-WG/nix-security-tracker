9f5a10b55b3b34e0d427119ae9b60e65c278c859
fricklerhandwerk <valentin@fricklerhandwerk.de>
2023-12-11 02:31:56 +0100

    feat: display knownVulnerabilities per derivation

--- src/website/webview/views.py
+++ src/website/webview/views.py
@@ -1,6 +1,8 @@
+import re
+
 from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, ListView, TemplateView
-from shared.models import NixpkgsIssue
+from shared.models import Container, CveRecord, NixpkgsIssue
 
 
 class HomeView(TemplateView):
@@ -11,8 +13,29 @@ class NixpkgsIssueView(DetailView):
     template_name = "issue_detail.html"
     model = NixpkgsIssue
 
+    pattern = re.compile(CveRecord._meta.get_field("cve_id").validators[0].regex)
+
     def get_object(self, queryset=None):
-        return get_object_or_404(self.model, **{"code": self.kwargs.get("code")})
+        issue = get_object_or_404(self.model, code=self.kwargs.get("code"))
+        derivations = issue.derivations.all()  # type: ignore
+        for drv in derivations:
+            result = self.get_cves_for_derivation(drv)
+            drv.known_cves = result
+
+        issue.derivations_with_cves = derivations  # type: ignore
+
+        return issue
+
+    def get_cves_for_derivation(self, drv):
+        known_vulnerabilities = drv.metadata.known_vulnerabilities
+        if not known_vulnerabilities:
+            return None
+        cves = [s for s in known_vulnerabilities if self.pattern.match(s)]
+        existing_cves = Container.objects.filter(cve__cve_id__in=cves)
+        if not existing_cves:
+            return None
+        else:
+            return existing_cves
 
 
 class NixpkgsIssueListView(ListView):
