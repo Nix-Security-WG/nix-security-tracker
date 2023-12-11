58aa9815d7c0d077257c8b0729e5c7bdee16db20
fricklerhandwerk <valentin@fricklerhandwerk.de>
2023-12-11 13:35:18 +0100

    refactor: move custom Python packages creation into subdir

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,22 +1,7 @@
 { pkgs, python3 }:
 
-let
-  inherit (pkgs) lib;
+{
 
-  callPackage = lib.callPackageWith (pkgs // { inherit python3; } // python3.pkgs // pythonPackages);
-
-  mkPackages =
-    dir:
-    with builtins;
-    listToAttrs (
-      map
-        (name: {
-          inherit name;
-          value = callPackage (dir + "/${name}") { };
-        })
-        (attrNames (readDir dir))
-    );
-
-  pythonPackages = mkPackages ./python;
-in
-pythonPackages
+  # place more custom packages here
+  python3Packages = pkgs.callPackage ./python { inherit python3; };
+}
