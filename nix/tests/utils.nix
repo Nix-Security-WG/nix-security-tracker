17a3d1e2916c47979d9501a5b223fd36ef3521d0
Jörg Thalheim <joerg@thalheim.io>
2023-12-12 10:20:53 +0100

    ci: use nixos test module system and speed up nixos tests bit
    This one is more flexible because it exposes us the whole module system
    for configuration. We safe some time by not evaluating & downloading documentation.

--- nix/tests/utils.nix
+++ nix/tests/utils.nix
@@ -1,45 +1,40 @@
-{
-  pkgs,
-  wstModule,
-  defaultTimeout ? 60 * 1024,
-}:
+{ pkgs, wstModule }:
+let
+  nixos-lib = import (pkgs.path + "/nixos/lib") { };
+in
 {
   mkVMTest =
-    {
-      name,
-      machine ? { },
-      testScript ? "",
-      ...
-    }:
-    pkgs.nixosTest {
-      inherit name;
-      globalTimeout = defaultTimeout;
-
-      testScript = ''
+    test:
+    (nixos-lib.runTest {
+      # speed-up evaluation
+      hostPkgs = pkgs;
+      globalTimeout = 60 * 1024;
+      testScript = pkgs.lib.mkBefore ''
         machine.wait_for_unit("multi-user.target")
         machine.wait_for_unit("web-security-tracker-server.service")
         machine.wait_for_open_port(8000)
         machine.succeed("curl http://127.0.0.1:8000")
         machine.succeed("wst-manage test")
-        ${testScript}
       '';
+      defaults = {
+        nixpkgs.pkgs = pkgs;
+        documentation.enable = pkgs.lib.mkDefault false;
 
-      nodes.machine =
-        { ... }:
-        {
-          imports = [
-            wstModule
-            machine
-          ];
+        virtualisation = {
+          memorySize = 2048;
+          cores = 2; # github runner comes with 2 cores
+        };
 
-          services.web-security-tracker = {
-            enable = true;
-            secrets = {
-              SECRET_KEY = pkgs.writeText "secret.key" "aaaaaaaaaaaaaaaaaaaa";
-              GH_CLIENT_ID = pkgs.writeText "gh_client" "bonjour";
-              GH_SECRET = pkgs.writeText "gh_secret" "secret";
-            };
+        imports = [ wstModule ];
+        services.web-security-tracker = {
+          enable = true;
+          secrets = {
+            SECRET_KEY = pkgs.writeText "secret.key" "aaaaaaaaaaaaaaaaaaaa";
+            GH_CLIENT_ID = pkgs.writeText "gh_client" "bonjour";
+            GH_SECRET = pkgs.writeText "gh_secret" "secret";
           };
         };
-    };
+      };
+      imports = [ test ];
+    }).config.result;
 }
