8661436a30d1c84f1cba515dd3743e7eeb51190c
Jörg Thalheim <joerg@thalheim.io>
2023-12-13 10:07:30 +0100

    ci: make it possible to still build `tests`

--- default.nix
+++ default.nix
@@ -3,106 +3,106 @@
   overlay ? import ./nix/overlay.nix,
   pkgs ? import sources.nixpkgs { overlays = [ overlay ]; },
 }:
-rec {
-  inherit (pkgs) python3;
-  localPythonPackages = import ./pkgs { inherit pkgs python3; };
+let
+  self = rec {
+    inherit (pkgs) python3;
+    localPythonPackages = import ./pkgs { inherit pkgs python3; };
 
-  # For exports.
-  overlays = [ overlay ];
-  package = pkgs.web-security-tracker;
-  module = import ./nix/web-security-tracker.nix;
+    # For exports.
+    overlays = [ overlay ];
+    package = pkgs.web-security-tracker;
+    module = import ./nix/web-security-tracker.nix;
 
-  pre-commit-check = pkgs.pre-commit-hooks {
-    src = ./.;
+    pre-commit-check = pkgs.pre-commit-hooks {
+      src = ./.;
 
-    settings.statix.ignore = [
-      "/staging"
-      "/nix/web-security-tracker.nix"
-    ];
+      settings.statix.ignore = [
+        "/staging"
+        "/nix/web-security-tracker.nix"
+      ];
 
-    hooks =
-      let
-        pythonExcludes = [
-          "/migrations/" # auto-generated code
-        ];
-      in
-      {
-        # Nix setup
-        nixfmt = {
-          enable = true;
-          entry = pkgs.lib.mkForce "${pkgs.lib.getExe pkgs.nixfmt}";
-        };
-        statix.enable = true;
-        deadnix.enable = true;
-
-        # Python setup
-        ruff = {
-          enable = true;
-          excludes = pythonExcludes;
-        };
-        ruff-format = {
-          enable = true;
-          name = "Format python code with ruff";
-          types = [
-            "text"
-            "python"
+      hooks =
+        let
+          pythonExcludes = [
+            "/migrations/" # auto-generated code
           ];
-          entry = "${pkgs.lib.getExe pkgs.ruff} format";
-          excludes = pythonExcludes;
-        };
-
-        pyright =
-          let
-            pyEnv = pkgs.python3.withPackages (_: pkgs.web-security-tracker.propagatedBuildInputs);
-            wrappedPyright = pkgs.runCommand "pyright" { nativeBuildInputs = [ pkgs.makeWrapper ]; } ''
-              makeWrapper ${pkgs.pyright}/bin/pyright $out \
-                --set PYTHONPATH ${pyEnv}/${pyEnv.sitePackages} \
-                --prefix PATH : ${pyEnv}/bin \
-                --set PYTHONHOME ${pyEnv}
-            '';
-          in
-          {
+        in
+        {
+          # Nix setup
+          nixfmt = {
             enable = true;
-            entry = pkgs.lib.mkForce (builtins.toString wrappedPyright);
+            entry = pkgs.lib.mkForce "${pkgs.lib.getExe pkgs.nixfmt}";
+          };
+          statix.enable = true;
+          deadnix.enable = true;
+
+          # Python setup
+          ruff = {
+            enable = true;
+            excludes = pythonExcludes;
+          };
+          ruff-format = {
+            enable = true;
+            name = "Format python code with ruff";
+            types = [
+              "text"
+              "python"
+            ];
+            entry = "${pkgs.lib.getExe pkgs.ruff} format";
             excludes = pythonExcludes;
           };
 
-        # Global setup
-        prettier = {
-          enable = true;
-          excludes = [
-            "\\.min.css$"
-            "\\.html$"
-          ];
+          pyright =
+            let
+              pyEnv = pkgs.python3.withPackages (_: pkgs.web-security-tracker.propagatedBuildInputs);
+              wrappedPyright = pkgs.runCommand "pyright" { nativeBuildInputs = [ pkgs.makeWrapper ]; } ''
+                makeWrapper ${pkgs.pyright}/bin/pyright $out \
+                  --set PYTHONPATH ${pyEnv}/${pyEnv.sitePackages} \
+                  --prefix PATH : ${pyEnv}/bin \
+                  --set PYTHONHOME ${pyEnv}
+              '';
+            in
+            {
+              enable = true;
+              entry = pkgs.lib.mkForce (builtins.toString wrappedPyright);
+              excludes = pythonExcludes;
+            };
+
+          # Global setup
+          prettier = {
+            enable = true;
+            excludes = [
+              "\\.min.css$"
+              "\\.html$"
+            ];
+          };
+          commitizen.enable = true;
         };
-        commitizen.enable = true;
-      };
-  };
+    };
 
-  shell = pkgs.mkShell {
-    DATA_CACHE_DIRECTORY = toString ./. + "/.data_cache";
+    shell = pkgs.mkShell {
+      DATA_CACHE_DIRECTORY = toString ./. + "/.data_cache";
 
-    packages = [
-      package
-      pkgs.nix-eval-jobs
-      pkgs.commitizen
-      pkgs.npins
-      pkgs.nixfmt
-    ];
+      packages = [
+        package
+        pkgs.nix-eval-jobs
+        pkgs.commitizen
+        pkgs.npins
+        pkgs.nixfmt
+      ];
 
-    shellHook = ''
-      ${pre-commit-check.shellHook}
+      shellHook = ''
+        ${pre-commit-check.shellHook}
 
-      mkdir -p .credentials
-      export CREDENTIALS_DIRECTORY=${builtins.toString ./.credentials}
-    '';
-  };
+        mkdir -p .credentials
+        export CREDENTIALS_DIRECTORY=${builtins.toString ./.credentials}
+      '';
+    };
 
-  inherit
-    (import ./nix/tests/vm-basic.nix {
+    tests = import ./nix/tests/vm-basic.nix {
       inherit pkgs;
       wstModule = module;
-    })
-    vm-basic
-    ;
-}
+    };
+  };
+in
+self // self.tests
