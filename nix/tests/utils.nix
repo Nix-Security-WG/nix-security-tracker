069264538165deeaed7e3f2b49b2dd91cdcefbb2
Raito Bezarius <masterancpp@gmail.com>
2023-12-19 14:58:14 +0100

    feat: NixOS module is now aware of the bg job system and evaluation
    Rewire the evaluation settings and creates:
    
    - job processor systemd unit
    - fetch all channels cron

--- nix/tests/utils.nix
+++ nix/tests/utils.nix
@@ -12,6 +12,7 @@ in
       testScript = pkgs.lib.mkBefore ''
         machine.wait_for_unit("multi-user.target")
         machine.wait_for_unit("web-security-tracker-server.service")
+        machine.wait_for_unit("web-security-tracker-worker.service")
         machine.wait_for_open_port(8000)
         machine.succeed("curl --fail -H 'Host: example.org' http://127.0.0.1:80/static/style.css")
         machine.succeed("wst-manage test")
