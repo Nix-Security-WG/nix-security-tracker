cbd2abdef8ec77f0ac9d6ab7b5e5414d972c41c5
Raito Bezarius <masterancpp@gmail.com>
2023-12-19 14:58:14 +0100

    feat: NixOS module is now aware of the bg job system and evaluation
    Rewire the evaluation settings and creates:
    
    - job processor systemd unit
    - fetch all channels cron

--- nix/web-security-tracker.nix
+++ nix/web-security-tracker.nix
@@ -11,9 +11,11 @@ let
     mkEnableOption
     mkPackageOptionMD
     mkOption
+    mapAttrs
     mapAttrsToList
     mkDefault
     concatStringsSep
+    recursiveUpdate
     ;
   inherit (pkgs) writeScriptBin stdenv;
   cfg = config.services.web-security-tracker;
@@ -94,6 +96,10 @@ in
       type = types.attrsOf types.path;
       default = { };
     };
+    maxJobProcessors = mkOption {
+      type = types.int;
+      default = 3;
+    };
   };
 
   config = mkIf cfg.enable {
@@ -109,6 +115,9 @@ in
           "[::1]"
         ];
         CSRF_TRUSTED_ORIGINS = mkDefault [ "https://${cfg.domain}" ];
+        EVALUATION_GC_ROOTS_DIRECTORY = mkDefault "/var/lib/web-security-tracker/gc-roots";
+        EVALUATION_LOGS_DIRECTORY = mkDefault "/var/log/web-security-tracker/evaluation";
+        LOCAL_NIXPKGS_CHECKOUT = mkDefault "/var/lib/web-security-tracker/nixpkgs-repo";
       };
 
       nginx.enable = true;
@@ -144,83 +153,109 @@ in
     };
     users.groups.web-security-tracker = { };
 
-    systemd.services = {
-      web-security-tracker-server = {
-        description = "A web security tracker ASGI server";
-        after = [
-          "network.target"
-          "postgresql.service"
-        ];
-        requires = [ "postgresql.service" ];
-        wantedBy = [ "multi-user.target" ];
-        path = [
-          pythonEnv
-          wstManageScript
-        ];
-        serviceConfig = {
-          User = "web-security-tracker";
-          Restart = "always";
-          WorkingDirectory = "/var/lib/web-security-tracker";
-          StateDirectory = "web-security-tracker";
-          RuntimeDirectory = "web-security-tracker";
-          LoadCredential = credentials;
+    systemd.services =
+      let
+        defaults = {
+          path = [
+            pythonEnv
+            wstManageScript
+          ];
+          serviceConfig = {
+            User = "web-security-tracker";
+            WorkingDirectory = "/var/lib/web-security-tracker";
+            StateDirectory = "web-security-tracker";
+            RuntimeDirectory = "web-security-tracker";
+            LogsDirectory = "web-security-tracker";
+            LoadCredential = credentials;
+          };
+          environment = {
+            DATABASE_URL = databaseUrl;
+            USER_SETTINGS_FILE = "${configFile}";
+          };
         };
-        environment = {
-          DATABASE_URL = databaseUrl;
-          USER_SETTINGS_FILE = "${configFile}";
-        };
-        preStart = ''
-          # Auto-migrate on first run or if the package has changed
-          versionFile="/var/lib/web-security-tracker/package-version"
-          if [[ $(cat "$versionFile" 2>/dev/null) != ${cfg.package} ]]; then
-            wst-manage migrate --no-input
-            wst-manage collectstatic --no-input --clear
-            echo ${cfg.package} > "$versionFile"
-          fi
-        '';
-        script =
-          let
-            networking =
-              if cfg.unixSocket != null then "-u ${cfg.unixSocket}" else "-b 127.0.0.1 -p ${toString cfg.port}";
-          in
-          ''
-            daphne ${networking} \
-              tracker.asgi:application
+      in
+      mapAttrs (_: recursiveUpdate defaults) {
+        web-security-tracker-server = {
+          description = "A web security tracker ASGI server";
+          after = [
+            "network.target"
+            "postgresql.service"
+          ];
+          requires = [ "postgresql.service" ];
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig.Restart = "always";
+          preStart = ''
+            # Auto-migrate on first run or if the package has changed
+            versionFile="/var/lib/web-security-tracker/package-version"
+            if [[ $(cat "$versionFile" 2>/dev/null) != ${cfg.package} ]]; then
+              wst-manage migrate --no-input
+              wst-manage collectstatic --no-input --clear
+              echo ${cfg.package} > "$versionFile"
+            fi
           '';
-      };
-
-      web-security-tracker-delta = {
-        description = "Web security tracker catch up with CVEs";
-        after = [
-          "network.target"
-          "postgresql.service"
-          "web-security-tracker-server.service"
-        ];
-        requires = [ "postgresql.service" ];
-        path = [
-          pythonEnv
-          wstManageScript
-        ];
-        serviceConfig = {
-          User = "web-security-tracker";
-          WorkingDirectory = "/var/lib/web-security-tracker";
-          StateDirectory = "web-security-tracker";
-          RuntimeDirectory = "web-security-tracker";
-          LoadCredential = credentials;
-          Type = "oneshot";
-        };
-        environment = {
-          DATABASE_URL = databaseUrl;
-          USER_SETTINGS_FILE = "${configFile}";
+          script =
+            let
+              networking =
+                if cfg.unixSocket != null then "-u ${cfg.unixSocket}" else "-b 127.0.0.1 -p ${toString cfg.port}";
+            in
+            ''
+              daphne ${networking} \
+                tracker.asgi:application
+            '';
         };
 
-        script = ''
-          wst-manage ingest_delta_cve "$(date --date='yesterday' --iso)"
-        '';
+        web-security-tracker-worker = {
+          description = "Web security tracker - background job processor";
+          after = [
+            "network.target"
+            "postgresql.service"
+            "web-security-tracker-server.service"
+          ];
+          requires = [ "postgresql.service" ];
+          wantedBy = [ "multi-user.target" ];
 
-        # Start at 03h so that the data will have been published
-        startAt = "*-*-* 03:00:00";
+          script = ''
+            wst-manage listen --processes ${toString cfg.maxJobProcessors}
+          '';
+        };
+
+        web-security-tracker-fetch-all-channels = {
+          description = "Web security tracker - refresh all channels and start nixpkgs evaluation";
+
+          after = [
+            "network.target"
+            "postgresql.service"
+            "web-security-tracker-server.service"
+          ];
+          requires = [ "postgresql.service" ];
+
+          serviceConfig.Type = "oneshot";
+
+          script = ''
+            wst-manage fetch_all_channels
+          '';
+
+          # Ideally, start at whatever night means.
+          startAt = "*-*-* 04:00:00";
+        };
+
+        web-security-tracker-delta = {
+          description = "Web security tracker catch up with CVEs";
+          after = [
+            "network.target"
+            "postgresql.service"
+            "web-security-tracker-server.service"
+          ];
+          requires = [ "postgresql.service" ];
+          serviceConfig.Type = "oneshot";
+
+          script = ''
+            wst-manage ingest_delta_cve "$(date --date='yesterday' --iso)"
+          '';
+
+          # Start at 03h so that the data will have been published
+          startAt = "*-*-* 03:00:00";
+        };
       };
-    };
   };
 }
