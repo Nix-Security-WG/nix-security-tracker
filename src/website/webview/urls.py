ca4fce5808f6275047cf3305fd2d292d84a0b583
Alejandro Sanchez Medina <alejandrosanchzmedina@gmail.com>
2024-06-24 17:36:07 +0530

    feat: add basic search and filtering for triage view

--- src/website/webview/urls.py
+++ src/website/webview/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, re_path
 
-from webview.views import HomeView, NixpkgsIssueListView, NixpkgsIssueView, TriageView
+from webview.views import HomeView, NixpkgsIssueListView, NixpkgsIssueView, triage_view
 
 app_name = "webview"
 
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
-    path("triage/", TriageView.as_view(), name="triage_view"),
+    path("triage/", triage_view, name="triage_view"),
     path("issues/", NixpkgsIssueListView.as_view(), name="issue_list"),
     re_path(
         r"^issues/(?P<code>NIXPKGS-[0-9]{4}-[0-9]{4,19})$",
--- src/website/webview/views.py
+++ src/website/webview/views.py
@@ -2,12 +2,15 @@ import re
 from typing import Any
 
 from django.contrib.postgres.search import SearchVector
+from django.core.paginator import Paginator
 from django.db.models.manager import BaseManager
-from django.shortcuts import get_object_or_404
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, render
 from django.views.generic import DetailView, ListView, TemplateView
 from shared.models import (
     Container,
     CveRecord,
+    NixDerivation,
     NixpkgsIssue,
 )
 
@@ -16,22 +19,26 @@ class HomeView(TemplateView):
     template_name = "home_view.html"
 
 
-class TriageView(ListView):
+def triage_view(request: HttpRequest) -> HttpResponse:
     template_name = "triage_view.html"
-    model = Container
     paginate_by = 25
 
-    def get_queryset(self) -> BaseManager[Container]:
-        qs = (
-            Container.objects.prefetch_related("descriptions", "affected", "cve")
-            .exclude(title="")
-            .order_by("id", "-date_public")
-        )
-        search_query = self.request.GET.get("search_query")
-        if not search_query:
-            return qs.all()
-        else:
-            return qs.annotate(
+    cve_qs = (
+        Container.objects.prefetch_related("descriptions", "affected", "cve")
+        .exclude(title="")
+        .order_by("id", "-date_public")
+    )
+    pkg_qs = NixDerivation.objects.prefetch_related("metadata").order_by("id")
+    cve_objects = cve_qs.all()
+    pkg_objects = pkg_qs.all()
+
+    # Fetch query parameters
+    search_cves = request.GET.get("search_cves")
+    search_pkgs = request.GET.get("search_pkgs")
+
+    if search_cves:
+        cve_objects = (
+            cve_qs.annotate(
                 search=SearchVector(
                     "title",
                     "descriptions__value",
@@ -41,7 +48,43 @@ class TriageView(ListView):
                     "affected__repo",
                     "affected__cpes__name",
                 )
-            ).distinct("id")
+            )
+            .filter(search=search_cves)
+            .distinct("id")
+        )
+
+    if search_pkgs:
+        pkg_objects = (
+            pkg_qs.annotate(
+                search=SearchVector(
+                    "attribute",
+                    "name",
+                    "system",
+                    "metadata__name",
+                    "metadata__description",
+                )
+            )
+            .filter(search=search_pkgs)
+            .distinct("id")
+        )
+
+    # Paginators
+    cve_paginator = Paginator(cve_objects, paginate_by)
+    cve_page_number = 1  # request.GET.get('page_cves', 1)
+    cve_page_objects = cve_paginator.get_page(cve_page_number)
+
+    pkg_paginator = Paginator(pkg_objects, paginate_by)
+    pkg_page_number = 1  # request.GET.get('page_pkgs', 1)
+    pkg_page_objects = pkg_paginator.get_page(pkg_page_number)
+
+    context = {
+        "cve_list": cve_page_objects,
+        "pkg_list": pkg_page_objects,
+        "search_cves": search_cves,
+        "search_pkgs": search_pkgs,
+    }
+
+    return render(request, template_name, context)
 
 
 class NixpkgsIssueView(DetailView):
