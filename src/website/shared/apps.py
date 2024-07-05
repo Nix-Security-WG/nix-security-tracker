013e3f56112024ea9418acc9c77561a6bb480f49
Alejandro Sanchez Medina <alejandrosanchzmedina@gmail.com>
2024-07-05 15:00:59 +0200

    fix: register third party admin views in custom admin site

--- src/website/shared/apps.py
+++ src/website/shared/apps.py
@@ -17,3 +17,27 @@ class SharedConfig(AppConfig):
 
         # Initialize group memberships when a user first logs in via Github.
         post_save.connect(init_user_groups, sender=SocialAccount)
+
+        # Update admin registry
+        """
+        Update the custom admin site registry with the contrib's admin site registry entries.
+
+        Strangely, third party apps are not registering their admin views to the custom admin site,
+        despite it being defined as the default admin site.
+
+        From the perspective of the 'tracker' project, the init code below enters an infinite recursion
+        when referencing `admin.site._registry`, which means there is a cirucular reference and third 
+        party apps should be pointed at the right admin site by default in normal cicumstances.
+        '''
+            from django.contrib import admin
+            ...
+            class CustomAdminSite(admin.AdminSite):
+                def __init__(self, *args, **kwargs):
+                    super(CustomAdminSite, self).__init__(*args, **kwargs)
+                    self._registry.update(admin.site._registry)
+        '''
+        """
+        from django.contrib.admin import site
+        from tracker.admin import custom_admin_site
+
+        custom_admin_site._registry.update(site._registry)
