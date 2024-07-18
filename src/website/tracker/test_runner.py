2ee288415d545fd59a34ad4319b9c6811446692a
Alejandro Sánchez Medina <alejandrosanchzmedina@gmail.com>
2024-07-18 07:46:29 -0700

    apply comment improvements from code review
    It should have been added to the previous batched commit, but we'll squash them anyway.
    
    Co-authored-by: Valentin Gagarin <valentin.gagarin@tweag.io>

--- src/website/tracker/test_runner.py
+++ src/website/tracker/test_runner.py
@@ -12,5 +12,7 @@ from django.test.runner import DiscoverRunner
 
 class CustomTestRunner(DiscoverRunner):
     def run_tests(self, *args: Any, **kwargs: dict[str, Any]) -> Any:
+        # ensure tests are isolated, this is important when running them in the development environment
+        # this library may be overkill, replace it with something more lightweight if possible
         with httpretty.enabled(allow_net_connect=False):
             return super().run_tests(*args, **kwargs)
