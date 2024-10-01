880ee1fbae9ee83787549b692822c287783b22b8
Valentin Gagarin <valentin@gagarin.work>
2024-10-01 22:56:29 +0200

    refactor: simplify default.nix attrset
    the diff looks huge, but semantically this is a tiny change:
    add `lib` to the top-level arguments and don't merge tests separately
    since they're already part of the `rec` attrset.

--- default.nix
+++ default.nix
@@ -7,127 +7,125 @@
     overlays = [ overlay ];
     inherit system;
   },
+  lib ? import "${sources.nixpkgs}/lib",
 }:
-let
-  self = rec {
-    inherit pkgs;
-    inherit (pkgs) python3;
-    localPythonPackages = import ./pkgs { inherit pkgs python3; };
+rec {
+  inherit pkgs;
+  inherit (pkgs) python3;
+  localPythonPackages = import ./pkgs { inherit pkgs python3; };
 
-    # For exports.
-    overlays = [ overlay ];
-    package = pkgs.web-security-tracker;
-    module = import ./nix/web-security-tracker.nix;
-    dev-container = import ./staging/container.nix;
+  # For exports.
+  overlays = [ overlay ];
+  package = pkgs.web-security-tracker;
+  module = import ./nix/web-security-tracker.nix;
+  dev-container = import ./staging/container.nix;
 
-    pre-commit-check = pkgs.pre-commit-hooks {
-      src = ./.;
+  pre-commit-check = pkgs.pre-commit-hooks {
+    src = ./.;
 
-      hooks =
-        let
-          pythonExcludes = [
-            "/migrations/" # auto-generated code
-          ];
-        in
-        {
-          # Nix setup
-          nixfmt-rfc-style.enable = true;
-          statix = {
-            enable = true;
-            settings.ignore = [ "staging" ];
-          };
-          deadnix.enable = true;
-
-          # Python setup
-          ruff = {
-            enable = true;
-            excludes = pythonExcludes;
-          };
-          ruff-format = {
-            enable = true;
-            name = "Format python code with ruff";
-            types = [
-              "text"
-              "python"
-            ];
-            entry = "${pkgs.lib.getExe pkgs.ruff} format";
-            excludes = pythonExcludes;
-          };
-
-          pyright =
-            let
-              pyEnv = pkgs.python3.withPackages (_: pkgs.web-security-tracker.propagatedBuildInputs);
-              wrappedPyright = pkgs.runCommand "pyright" { nativeBuildInputs = [ pkgs.makeWrapper ]; } ''
-                makeWrapper ${pkgs.pyright}/bin/pyright $out \
-                  --set PYTHONPATH ${pyEnv}/${pyEnv.sitePackages} \
-                  --prefix PATH : ${pyEnv}/bin \
-                  --set PYTHONHOME ${pyEnv}
-              '';
-            in
-            {
-              enable = true;
-              entry = pkgs.lib.mkForce (builtins.toString wrappedPyright);
-              excludes = pythonExcludes;
-            };
-
-          # Global setup
-          prettier = {
-            enable = true;
-            excludes = [
-              "\\.min.css$"
-              "\\.html$"
-            ] ++ pythonExcludes;
-          };
-          commitizen.enable = true;
-        };
-    };
-
-    shell =
+    hooks =
       let
-        manage = pkgs.writeScriptBin "manage" ''
-          ${python3}/bin/python ${toString ./src/website/manage.py} $@
-        '';
-        create-credentials = pkgs.writeShellApplication {
-          name = "create-credentials";
-          text = ''
-            if [ ! -d .credentials ]; then
-              mkdir .credentials
-              echo foo > .credentials/SECRET_KEY
-              echo bar > .credentials/GH_CLIENT_ID
-              echo baz > .credentials/GH_SECRET
-              echo qux > .credentials/GH_WEBHOOK_SECRET
-            fi
-          '';
-        };
+        pythonExcludes = [
+          "/migrations/" # auto-generated code
+        ];
       in
-      pkgs.mkShell {
-        REDIS_SOCKET_URL = "unix:///run/redis/redis.sock";
-        # `./src/website/tracker/settings.py` by default looks for LOCAL_NIXPKGS_CHECKOUT
-        # in the root of the repo. Make it the default here for local development.
-        LOCAL_NIXPKGS_CHECKOUT = toString ./. + "/nixpkgs";
+      {
+        # Nix setup
+        nixfmt-rfc-style.enable = true;
+        statix = {
+          enable = true;
+          settings.ignore = [ "staging" ];
+        };
+        deadnix.enable = true;
 
-        packages = [
-          manage
-          package
-          pkgs.nix-eval-jobs
-          pkgs.npins
-          pkgs.hivemind
-        ] ++ pre-commit-check.enabledPackages;
+        # Python setup
+        ruff = {
+          enable = true;
+          excludes = pythonExcludes;
+        };
+        ruff-format = {
+          enable = true;
+          name = "Format python code with ruff";
+          types = [
+            "text"
+            "python"
+          ];
+          entry = "${pkgs.lib.getExe pkgs.ruff} format";
+          excludes = pythonExcludes;
+        };
 
-        shellHook = ''
-          ${pre-commit-check.shellHook}
-          ${pkgs.lib.getExe create-credentials}
+        pyright =
+          let
+            pyEnv = pkgs.python3.withPackages (_: pkgs.web-security-tracker.propagatedBuildInputs);
+            wrappedPyright = pkgs.runCommand "pyright" { nativeBuildInputs = [ pkgs.makeWrapper ]; } ''
+              makeWrapper ${pkgs.pyright}/bin/pyright $out \
+                --set PYTHONPATH ${pyEnv}/${pyEnv.sitePackages} \
+                --prefix PATH : ${pyEnv}/bin \
+                --set PYTHONHOME ${pyEnv}
+            '';
+          in
+          {
+            enable = true;
+            entry = lib.mkForce (builtins.toString wrappedPyright);
+            excludes = pythonExcludes;
+          };
 
-          mkdir -p .credentials
-          export DATABASE_URL=postgres:///nix-security-tracker
-          export CREDENTIALS_DIRECTORY=${builtins.toString ./.credentials}
+        # Global setup
+        prettier = {
+          enable = true;
+          excludes = [
+            "\\.min.css$"
+            "\\.html$"
+          ] ++ pythonExcludes;
+        };
+        commitizen.enable = true;
+      };
+  };
+
+  shell =
+    let
+      manage = pkgs.writeScriptBin "manage" ''
+        ${python3}/bin/python ${toString ./src/website/manage.py} $@
+      '';
+      create-credentials = pkgs.writeShellApplication {
+        name = "create-credentials";
+        text = ''
+          if [ ! -d .credentials ]; then
+            mkdir .credentials
+            echo foo > .credentials/SECRET_KEY
+            echo bar > .credentials/GH_CLIENT_ID
+            echo baz > .credentials/GH_SECRET
+            echo qux > .credentials/GH_WEBHOOK_SECRET
+          fi
         '';
       };
+    in
+    pkgs.mkShell {
+      REDIS_SOCKET_URL = "unix:///run/redis/redis.sock";
+      # `./src/website/tracker/settings.py` by default looks for LOCAL_NIXPKGS_CHECKOUT
+      # in the root of the repo. Make it the default here for local development.
+      LOCAL_NIXPKGS_CHECKOUT = toString ./. + "/nixpkgs";
 
-    tests = import ./nix/tests/vm-basic.nix {
-      inherit pkgs;
-      wstModule = module;
+      packages = [
+        manage
+        package
+        pkgs.nix-eval-jobs
+        pkgs.npins
+        pkgs.hivemind
+      ] ++ pre-commit-check.enabledPackages;
+
+      shellHook = ''
+        ${pre-commit-check.shellHook}
+        ${lib.getExe create-credentials}
+
+        mkdir -p .credentials
+        export DATABASE_URL=postgres:///nix-security-tracker
+        export CREDENTIALS_DIRECTORY=${builtins.toString ./.credentials}
+      '';
     };
+
+  tests = import ./nix/tests/vm-basic.nix {
+    inherit pkgs;
+    wstModule = module;
   };
-in
-self // self.tests
+}
