dddfad7df98cd5fa7c066067bf6e7e0dfda9e7e8
Raito Bezarius <masterancpp@gmail.com>
2024-10-02 17:09:41 +0200

    feat(mgmt/ingest_bulk_cve): for each CVE made, log a single dot
    This is a barebone progress bar.
    
    Signed-off-by: Raito Bezarius <masterancpp@gmail.com>

--- src/website/shared/management/commands/ingest_bulk_cve.py
+++ src/website/shared/management/commands/ingest_bulk_cve.py
@@ -120,6 +120,7 @@ class Command(BaseCommand):
             for j_cve in cve_list:
                 with open(j_cve) as fc:
                     make_cve(json.load(fc), triaged=True)
+                    print(".", end="")
 
         if not path.exists(cve_data_cache_dir):
             # Record the ingestion
