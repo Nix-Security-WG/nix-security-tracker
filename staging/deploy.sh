d1a4be7e9e60c39fd0e1fad0218bbc778bcb7a98
Raito Bezarius <masterancpp@gmail.com>
2024-10-03 11:06:47 +0200

    fix(ops/staging): improve significantly the deployment script
    Wow, we were `test`-ing all the time and never persisting the changes…
    
    We perform dry-activation then actually switch to the configuration.
    
    Signed-off-by: Raito Bezarius <masterancpp@gmail.com>

--- staging/deploy.sh
+++ staging/deploy.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+set -eo pipefail
+
 DIR=$(dirname $DIRENV_FILE)
+VERB=${1:-switch}
+
+# Perform a dry-activation first.
+echo "dry-activating the configuration first..."
+nixos-rebuild dry-activate -I nixos-config=$DIR/staging/configuration.nix --target-host root@sectracker.nixpkgs.lahfa.xyz
 
 # This requires IPv6 to work as SSH is only IPv6-only.
 # Sorry, not sorry.
-nixos-rebuild test -I nixos-config=$DIR/staging/configuration.nix --target-host root@sectracker.nixpkgs.lahfa.xyz
+echo "$VERB-ing the configuration now."
+nixos-rebuild $VERB -I nixos-config=$DIR/staging/configuration.nix --target-host root@sectracker.nixpkgs.lahfa.xyz
