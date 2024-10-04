9dd53f6c9481873d47a1c300fe92b4e088aa168e
Raito Bezarius <masterancpp@gmail.com>
2024-10-04 16:55:14 +0200

    feat(ops/staging): skip dry-activation for build
    As we don't have access to the environment when we just build, let's not
    do it.
    
    Signed-off-by: Raito Bezarius <masterancpp@gmail.com>

--- staging/deploy.sh
+++ staging/deploy.sh
@@ -6,9 +6,13 @@ set -eo pipefail
 DIR=$(git rev-parse --show-toplevel)
 VERB=${1:-switch}
 
-# Perform a dry-activation first.
-echo "dry-activating the configuration first..."
-nixos-rebuild dry-activate -I nixos-config=$DIR/staging/configuration.nix --target-host root@sectracker.nixpkgs.lahfa.xyz
+if [[ "$VERB" != "build" ]]; then
+  # Perform a dry-activation first.
+  echo "dry-activating the configuration first..."
+  nixos-rebuild dry-activate -I nixos-config=$DIR/staging/configuration.nix --target-host root@sectracker.nixpkgs.lahfa.xyz
+else
+  echo "skipping the dry-activation as we are using an offline verb."
+fi
 
 # This requires IPv6 to work as SSH is only IPv6-only.
 # Sorry, not sorry.
