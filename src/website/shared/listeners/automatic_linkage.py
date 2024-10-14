3651c9a26d22e9d9ce0b83962dfb650f2199941b
Raito Bezarius <masterancpp@gmail.com>
2024-10-14 15:38:57 +0200

    feat: introduce provenance for derivation candidates
    In the future, we will be able to bolt on more fields to take notes
    which embedding has been used, which search term has been used, etc,
    etc.
    
    For now, we have a provenance flags we can use to hint the frontend what
    has been used.
    
    The dance for the M2M is required because it's not trivial to migrate
    everything.
    
    Progress towards:
    
    - https://github.com/Nix-Security-WG/nix-security-tracker/issues/189
    - https://github.com/Nix-Security-WG/nix-security-tracker/issues/274
    
    Signed-off-by: Raito Bezarius <masterancpp@gmail.com>

--- src/website/shared/listeners/automatic_linkage.py
+++ src/website/shared/listeners/automatic_linkage.py
@@ -4,25 +4,32 @@ import pgpubsub
 
 from shared.channels import ContainerChannel
 from shared.models.cve import Container
-from shared.models.linkage import CVEDerivationClusterProposal
+from shared.models.linkage import CVEDerivationClusterProposal, ProvenanceFlags
 from shared.models.nix_evaluation import NixDerivation
 
 logger = logging.getLogger(__name__)
 
 
-def produce_linkage_candidates(container: Container) -> set[NixDerivation]:
+def produce_linkage_candidates(
+    container: Container,
+) -> dict[NixDerivation, ProvenanceFlags]:
     # Methodology:
     # We start with a large list and we remove things as we sort out that list.
     # Our initialization must be as large as possible.
-    candidates = set()
+    candidates: dict[NixDerivation, ProvenanceFlags] = {}
     for affected in container.affected.all():
         # TODO: record what is used to expand the candidate list.
         if affected.package_name is not None:
-            candidates |= set(
+            drvs = set(
                 # TODO: improve accuracy by performing case normalization.
                 # TODO: improve accuracy by using bigrams similarity with a `| Q(...)` query.
                 NixDerivation.objects.filter(name__contains=affected.package_name)
             )
+            for d in drvs:
+                if d in candidates:
+                    candidates[d] |= ProvenanceFlags.PACKAGE_NAME_MATCH
+                else:
+                    candidates[d] = ProvenanceFlags.PACKAGE_NAME_MATCH
 
         # TODO: restrain further the list by checking all version constraints.
         # TODO: restrain further the list by checking hardware constraints or kernel constraints.
@@ -52,9 +59,9 @@ def build_new_links(container: Container) -> None:
 
     drvs_throughs = [
         CVEDerivationClusterProposal.derivations.through(
-            cvederivationclusterproposal_id=proposal.pk, nixderivation_id=drv.pk
+            proposal_id=proposal.pk, derivation_id=drv.pk, provenance_flags=flags
         )
-        for drv in drvs
+        for drv, flags in drvs.items()
     ]
 
     # We create all the set in one shot.
