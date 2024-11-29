3a160a49ab2010c1d6416f6eae5c3fa87f6c8188
Ryan Lahfa <masterancpp@gmail.com>
2024-11-29 14:58:10 +0100

    feat(shared/apps): run github sync state in production (#394)
    We just create a bespoke environment variable set by our packaging.
    
    There's no other way as we cannot depend on any specific WSGI/ASGI
    server argv0 here.
    
    Fixes #239.
    
    Signed-off-by: Raito Bezarius <masterancpp@gmail.com>

--- src/website/shared/apps.py
+++ src/website/shared/apps.py
@@ -13,7 +13,11 @@ class SharedConfig(AppConfig):
 
         # This hook is called on any `manage` subcommand.
         # Only connect to GitHub when the server is started.
-        if os.environ.get("RUN_MAIN", None) is None and "runserver" in sys.argv:
+        # TODO: run this as a separate service, as this is almost exclusively a deployment concern
+        if os.environ.get("RUN_MAIN", None) is None and (
+            "runserver" in sys.argv
+            or os.environ.get("SYNC_GITHUB_STATE_AT_STARTUP", False)
+        ):
             from shared.auth.github_state import GithubState
 
             self.github_state = GithubState()
