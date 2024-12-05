c6d3a1b1b39814b3eada9dc9d3712ea98f89e064
Valentin Gagarin <valentin@gagarin.work>
2024-12-05 22:12:53 +0100

    refactor: move everything into the toplevel

---
+++ infra/shell.nix
@@ -0,0 +1,11 @@
+{
+  sources ? import ../../npins,
+  system ? builtins.currentSystem,
+  pkgs ? import sources.nixpkgs {
+    config = { };
+    overlays = [ ];
+    inherit system;
+  },
+}:
+
+pkgs.mkShell { buildInputs = with pkgs; [ opentofu ]; }
--- infra/terraform/shell.nix
+++
@@ -1,3 +0,0 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-pkgs.mkShell { buildInputs = with pkgs; [ opentofu ]; }
