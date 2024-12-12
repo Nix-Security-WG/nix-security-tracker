f31260c190f06bf0d1d8b265a3289ceec8dccaa2
Alejandro Sánchez Medina <alejandrosanchzmedina@gmail.com>
2024-12-12 11:41:17 -0300

    feat(suggestions): auth checks for update actions

--- src/website/webview/templatetags/viewutils.py
+++ src/website/webview/templatetags/viewutils.py
@@ -4,6 +4,7 @@ from typing import Any, TypedDict, cast
 
 from django import template
 from django.template.context import Context
+from shared.auth import isadmin, ismaintainer
 from shared.listeners.cache_suggestions import parse_drv_name
 from shared.models.cve import AffectedProduct
 from shared.models.linkage import (
@@ -39,6 +40,7 @@ class PackageList(TypedDict):
 
 class PackageListContext(TypedDict):
     packages: PackageList
+    user_is_allowed_to_select: bool
 
 
 class AffectedContext(TypedDict):
@@ -102,6 +104,26 @@ def versioned_package_name(package_entry: str) -> str:
     return f"pkgs.{fields["attribute"]} {version}"
 
 
+def is_admin(user: Any) -> bool:
+    if user is None:
+        return False
+    else:
+        return isadmin(user)
+
+
+@register.filter
+def is_maintainer(user: Any) -> bool:
+    if user is None:
+        return False
+    else:
+        return ismaintainer(user)
+
+
+@register.filter
+def is_maintainer_or_admin(user: Any) -> bool:
+    return is_maintainer(user) or is_admin(user)
+
+
 @register.inclusion_tag("components/suggestion.html", takes_context=True)
 def suggestion(
     context: Context,
@@ -115,6 +137,7 @@ def suggestion(
         "activity_log": activity_log,
         "status_filter": context["status_filter"],
         "page_obj": context["page_obj"],
+        "user": context["user"],
     }
 
 
@@ -124,7 +147,9 @@ def nixpkgs_package(attribute_name: str, pdata: Package) -> PackageContext:
 
 
 @register.inclusion_tag("components/selectable_nixpkgs_package_list.html")
-def selectable_nixpkgs_package_list(packages: PackageList) -> PackageListContext:
+def selectable_nixpkgs_package_list(
+    packages: PackageList, user_is_allowed_to_select: bool
+) -> PackageListContext:
     """Renders the nixpkgs package list with additional checkboxes to have packages selectable.
 
     Args:
@@ -136,7 +161,10 @@ def selectable_nixpkgs_package_list(packages: PackageList) -> PackageListContext
     Example:
         {% package_list package_dict %}
     """
-    return {"packages": packages}
+    return {
+        "packages": packages,
+        "user_is_allowed_to_select": user_is_allowed_to_select,
+    }
 
 
 @register.inclusion_tag("components/nixpkgs_package_list.html")
@@ -152,7 +180,10 @@ def nixpkgs_package_list(packages: PackageList) -> PackageListContext:
     Example:
         {% package_list package_dict %}
     """
-    return {"packages": packages}
+    return {
+        "packages": packages,
+        "user_is_allowed_to_select": False,
+    }
 
 
 @register.inclusion_tag("components/affected_products.html")
