43c211b58dce135f23e9ba44d446165a1ca39d9d
Dionysis Grigoropoulos <dgrig@erethon.com>
2025-01-08 22:15:24 +0200

    infra: Migrate jobs/scripts to new staging host

--- default.nix
+++ default.nix
@@ -98,20 +98,10 @@ rec {
     let
       deploy = pkgs.writeShellApplication {
         name = "deploy";
-        text = builtins.readFile ./staging/deploy.sh;
+        text = builtins.readFile ./infra/deploy.sh;
         runtimeInputs = with pkgs; [
           nixos-rebuild
           coreutils
-        ];
-        # TODO: satisfy shellcheck
-        checkPhase = "";
-      };
-      dump-database = pkgs.writeShellApplication {
-        name = "dump-database";
-        text = builtins.readFile ./staging/dump-database.sh;
-        runtimeInputs = with pkgs; [
-          awscli
-          pv
         ];
         # TODO: satisfy shellcheck
         checkPhase = "";
@@ -121,7 +111,6 @@ rec {
       packages = [
         pkgs.npins
         deploy
-        dump-database
       ];
     };
 
@@ -130,7 +119,7 @@ rec {
       manage = pkgs.writeScriptBin "manage" ''
         ${python3}/bin/python ${toString ./src/website/manage.py} $@
       '';
-      deploymentSources = import ./staging/npins;
+      deploymentSources = import ./infra/npins;
     in
     pkgs.mkShellNoCC {
       env = {
