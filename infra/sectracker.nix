0423ede51aeafe0f8a29ae1bd72344e0bbf260d4
Valentin Gagarin <valentin@gagarin.work>
2025-04-24 11:10:16 +0200

    chore: satisfy linter

--- infra/sectracker.nix
+++ infra/sectracker.nix
@@ -1,7 +1,6 @@
 {
   config,
   pkgs,
-  lib,
   ...
 }:
 let
@@ -11,38 +10,40 @@ in
   imports = [ sectracker.module ];
 
   nixpkgs.overlays = sectracker.overlays;
-  services.nginx = {
-    enable = true;
-    recommendedTlsSettings = true;
-    recommendedProxySettings = true;
-    recommendedGzipSettings = true;
-    recommendedOptimisation = true;
-  };
-  services.postgresql = {
-    enableJIT = true;
-    settings = {
-      # Derived using PGTune for an 8 core, 16GB RAM host
-      max_connections = 200;
-      shared_buffers = "4GB";
-      effective_cache_size = "12GB";
-      maintenance_work_mem = "1GB";
-      checkpoint_completion_target = "0.9";
-      wal_buffers = "16MB";
-      default_statistics_target = "100";
-      random_page_cost = "1.1";
-      effective_io_concurrency = "200";
-      work_mem = "5242kB";
-      huge_pages = "off";
-      min_wal_size = "1GB";
-      max_wal_size = "4GB";
-      max_worker_processes = "8";
-      max_parallel_workers_per_gather = "4";
-      max_parallel_workers = "8";
-      max_parallel_maintenance_workers = "4";
+  services = {
+    nginx = {
+      enable = true;
+      recommendedTlsSettings = true;
+      recommendedProxySettings = true;
+      recommendedGzipSettings = true;
+      recommendedOptimisation = true;
     };
-    authentication = ''
-      local all all trust
-    '';
+    postgresql = {
+      enableJIT = true;
+      settings = {
+        # Derived using PGTune for an 8 core, 16GB RAM host
+        max_connections = 200;
+        shared_buffers = "4GB";
+        effective_cache_size = "12GB";
+        maintenance_work_mem = "1GB";
+        checkpoint_completion_target = "0.9";
+        wal_buffers = "16MB";
+        default_statistics_target = "100";
+        random_page_cost = "1.1";
+        effective_io_concurrency = "200";
+        work_mem = "5242kB";
+        huge_pages = "off";
+        min_wal_size = "1GB";
+        max_wal_size = "4GB";
+        max_worker_processes = "8";
+        max_parallel_workers_per_gather = "4";
+        max_parallel_workers = "8";
+        max_parallel_maintenance_workers = "4";
+      };
+      authentication = ''
+        local all all trust
+      '';
+    };
   };
   security.acme.acceptTerms = true;
   security.acme.defaults.email = "infra@nixos.org";
