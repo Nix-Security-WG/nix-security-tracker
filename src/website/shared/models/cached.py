4cdad2f77757b4fd201d3806ea14b6349001c080
Yann Hamdaoui <yann.hamdaoui@tweag.io>
2025-04-28 15:33:15 +0200

    feat: add all_maintainers() property to cached suggestions

--- src/website/shared/models/cached.py
+++ src/website/shared/models/cached.py
@@ -19,3 +19,28 @@ class CachedSuggestions(TimeStampMixin):
 
     # The exact format of this payload will change until it's properly defined.
     payload = models.JSONField(encoder=DjangoJSONEncoder)
+
+    @property
+    # TODO: for now we blindly list all maintainers of all affected packages,
+    # but in the future we might want to be able to edit this list before
+    # creating the GitHub issue. When that happens, this function will need to
+    # be updated (or the GitHub creation code should use a distinct
+    # `maintainers()` property, for example).
+    def all_maintainers(self) -> list[dict]:
+        """
+        Returns a deduplicated list (by GitHub ID) of all the maintainers of all
+        the affected packages linked to this suggestion.
+        """
+
+        seen = set()
+        result = []
+        all_maintainers = [
+            m for pkg in self.payload["packages"].values() for m in pkg["maintainers"]
+        ]
+
+        for m in all_maintainers:
+            if m["github_id"] not in seen:
+                seen.add(m["github_id"])
+                result.append(m)
+
+        return result
