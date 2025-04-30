49b42f97a9ac6cb428429a028189929769b09355
Yann Hamdaoui <yann.hamdaoui@tweag.io>
2025-04-30 17:54:10 +0200

    feat: user profile model (for subscriptions)

--- src/website/webview/models.py
+++ src/website/webview/models.py
@@ -1,1 +1,26 @@
 # Create your models here.
+from typing import Any
+
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from shared.models import NixpkgsIssue
+
+
+class Profile(models.Model):
+    """
+    Profile associated to a user, storing extra non-auth-related data such as
+    active issue subscriptions.
+    """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    subscriptions = models.ManyToManyField(NixpkgsIssue, related_name="subscribers")
+
+
+@receiver(post_save, sender=User)
+def create_profile(
+    sender: type[User], instance: User, created: bool, **kwargs: Any
+) -> None:
+    if created:
+        Profile.objects.create(user=instance)
