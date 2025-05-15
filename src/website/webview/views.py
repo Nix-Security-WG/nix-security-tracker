37de0ac3299eb5b50fbb44959f55a629b8ef9903
Florent C. <florentc@users.noreply.github.com>
2025-05-15 16:05:21 +0200

    fix: Formatting

--- src/website/webview/views.py
+++ src/website/webview/views.py
@@ -692,7 +692,7 @@ class SuggestionListView(ListView):
                     if tracker_issue:
                         changed_suggestion_link = f"/issues/{tracker_issue.code}"
                     else:
-                        changed_suggestion_link = f"/issues"
+                        changed_suggestion_link = "/issues"
                 else:
                     changed_suggestion_link = f"{self.status_route_dict[suggestion.status]}#suggestion-{suggestion.pk}"
                 snippet = render_to_string(
