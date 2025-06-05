827151f1c4e126a2e37d2d5aa845b884589b64b1
Florent C. <florentc@users.noreply.github.com>
2025-06-05 18:00:28 +0200

    feat: show git revision on every page

--- src/website/tracker/settings.py
+++ src/website/tracker/settings.py
@@ -330,6 +330,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "tracker.git_revision.git_revision",
             ],
         },
     },
