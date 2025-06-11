03732011b46fa58e85da7045c3996f626486f846
Valentin Gagarin <valentin.gagarin@tweag.io>
2025-06-11 20:50:14 +0200

    fix: remove unneeded

--- src/website/tracker/settings.py
+++ src/website/tracker/settings.py
@@ -337,7 +337,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "tracker.git_revision.git_revision",
             ],
         },
     },
