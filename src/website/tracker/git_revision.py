a69d4db77dfeb6226661a31965180c0ac1050ba0
Valentin Gagarin <valentin.gagarin@tweag.io>
2025-06-11 20:22:44 +0200

    fix: simplify

--- src/website/tracker/git_revision.py
+++ src/website/tracker/git_revision.py
@@ -1,14 +1,6 @@
 from django.conf import settings
-from django.core.cache import cache
 from django.http import HttpRequest
 
 
 def git_revision(request: HttpRequest) -> dict[str, str]:
-    revision = cache.get("git_revision")
-    if revision is None:
-        try:
-            revision = settings.REVISION
-        except Exception:
-            revision = "unknown"
-        cache.set("git_revision", revision, timeout=None)  # cache indefinitely
-    return {"git_revision": revision}
+    return {"git_revision": settings.REVISION}
