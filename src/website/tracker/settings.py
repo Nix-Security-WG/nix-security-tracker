fe4509006413fb72bc580712ee3a1623d2c7a90b
Valentin Gagarin <valentin.gagarin@tweag.io>
2025-06-13 11:39:13 +0200

    fix: show revision
    turns out I forgot to enable the context processor

--- src/website/tracker/settings.py
+++ src/website/tracker/settings.py
@@ -337,6 +337,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "shared.context_processors.git_revision",
             ],
         },
     },
