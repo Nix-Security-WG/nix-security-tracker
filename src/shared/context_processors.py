d9315110c86b8c3ed0bc3ea477071004ae31127a
Dionysis Grigoropoulos <dgrig@erethon.com>
2025-07-09 23:58:34 +0300

    chore: Update context processor name
    Git_revision is used for more variables than simply the revision ID, so
    change it something more descriptive

--- src/shared/context_processors.py
+++ src/shared/context_processors.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.http import HttpRequest
 
 
-def git_revision(request: HttpRequest) -> dict[str, str]:
+def deployment_info(request: HttpRequest) -> dict[str, str]:
     return {
         "production": settings.PRODUCTION,
         "git_revision": settings.REVISION,
