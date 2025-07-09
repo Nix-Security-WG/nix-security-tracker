c398344cff318809a34d0b9b00e8cd7bba2c65f8
Dionysis Grigoropoulos <dgrig@erethon.com>
2025-07-10 00:10:47 +0300

    infra: Show demo banner on production

--- infra/sectracker.nix
+++ infra/sectracker.nix
@@ -56,6 +56,7 @@ in
     production = true;
     domain = "tracker.security.nixos.org";
     env = {
+      SHOW_DEMO_DISCLAIMER = true;
       SYNC_GITHUB_STATE_AT_STARTUP = true;
       # set to `true` when going live
       GH_ISSUES_PING_MAINTAINERS = false;
