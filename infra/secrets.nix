b72907275f431508f98bf6afdcf5301df6e87abe
Dionysis Grigoropoulos <dgrig@erethon.com>
2025-10-24 02:00:58 +0300

    infra: Split staging config into seperate file
    Also rotate some GitHub secrets

--- infra/secrets.nix
+++ infra/secrets.nix
@@ -1,11 +1,11 @@
 let
-  prod = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEHib5Kk39PzPEheOf8fwIyeVbVgSzUiqUN2vSIXHO7N";
+  staging = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEHib5Kk39PzPEheOf8fwIyeVbVgSzUiqUN2vSIXHO7N";
 in
 {
-  "secrets/django-secret-key.age".publicKeys = [ prod ];
-  "secrets/gh-client.age".publicKeys = [ prod ];
-  "secrets/gh-secret.age".publicKeys = [ prod ];
-  "secrets/gh-webhook-secret.age".publicKeys = [ prod ];
-  "secrets/gh-app-installation-id.age".publicKeys = [ prod ];
-  "secrets/nixpkgs-security-tracker.2024-12-09.private-key.pem.age".publicKeys = [ prod ];
+  "secrets/staging-django-secret-key.age".publicKeys = [ staging ];
+  "secrets/staging-gh-client.age".publicKeys = [ staging ];
+  "secrets/staging-gh-secret.age".publicKeys = [ staging ];
+  "secrets/staging-gh-webhook-secret.age".publicKeys = [ staging ];
+  "secrets/staging-gh-app-installation-id.age".publicKeys = [ staging ];
+  "secrets/staging-nixpkgs-security-tracker.2024-12-09.private-key.pem.age".publicKeys = [ staging ];
 }
