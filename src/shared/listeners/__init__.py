0499df0332eeb51cab4831254607735c4d3c60b2
Florent C. <florentc@users.noreply.github.com>
2025-10-30 16:54:12 +0100

    fix: listener that creates user notifications wasn't being loaded

--- src/shared/listeners/__init__.py
+++ src/shared/listeners/__init__.py
@@ -2,3 +2,5 @@ import shared.listeners.nix_channels  # noqa
 import shared.listeners.nix_evaluation  # noqa
 import shared.listeners.automatic_linkage  # noqa
 import shared.listeners.cache_suggestions  # noqa
+import shared.listeners.notify_users  # noqa
+import shared.listeners.cache_issues  # noqa
